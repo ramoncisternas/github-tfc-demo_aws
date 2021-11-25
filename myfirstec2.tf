e112c5d5cbf4d0f38b5652b53da245cf559a2f4c
Ramon Cisternas <ramon.cisternas@axa.com>
2021-11-25 19:12:05 +0100

    initial commit

--- myfirstec2.tf
+++ myfirstec2.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.66.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
 resource "aws_instance" "mynewec2" {
   ami             = "ami-089b5384aac360007"
   instance_type   = "t2.micro"

f6f7780f0707a5622450dda6103891fc6fe61cbd
ramon-cisternas <ramon.cisternas@axa.com>
2021-11-26 11:35:05 +0100

    my-tf-test-bucket

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.66.0"
-    }
-  }
-}
-
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
-
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }

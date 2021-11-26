9a4bc058f5c21ca924fb0438ab298054ab18c6f8
ramon-cisternas <ramon.cisternas@axa.com>
2021-11-26 11:38:16 +0100

    rename to mi-tf-bucket-axa12345

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+  bucket = "mi-tf-bucket-axa12345"
   acl    = "private"
   tags = {
     Name        = "My bucket"
